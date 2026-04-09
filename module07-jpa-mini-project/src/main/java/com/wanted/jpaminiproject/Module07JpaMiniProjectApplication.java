fc1e10b95108db4a65a6a36194b5591d63527c64
sooyoung0927 <sooyoung040927@gmail.com>
2026-04-09 16:48:29 +0900

    Revert "jpa mini project"
    This reverts commit 8f1721fbd688eae142cb6049178811790828e335.

--- module07-jpa-mini-project/src/main/java/com/wanted/jpaminiproject/Module07JpaMiniProjectApplication.java
+++ module07-jpa-mini-project/src/main/java/com/wanted/jpaminiproject/Module07JpaMiniProjectApplication.java
@@ -1,4 +1,4 @@
-package com.wanted.jpaminiproject.global.config;
+package com.wanted.jpaminiproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
